bade0a0e7107846692cb73ab24e162e3fa26e539
ak <allan.koetter@live.com>
2022-12-31 10:26:18 +0100

    Sync ak@MBP02

--- src/data_mgmt/__init__.py
+++ src/data_mgmt/__init__.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 from dotenv import load_dotenv
 
@@ -8,3 +9,40 @@ __all__ = [
 from mgmt import *
 
 print("__init__.py was executed")
+=======
+
+__all__ = [
+        'mgmt',
+        'secrets'
+        ]
+
+def main(args=None):
+    compatible = True
+    if sys.version_info < (3, 7):
+        compatible = False
+    elif not hasattr(sys, 'base_prefix'):
+        compatible = False
+    if not compatible:
+        raise ValueError('This script is only for use with Python >= 3.7')
+    else:
+        import argparse
+        parser = argparse.ArgumentParser(
+                prog=__name__,
+                description='Creates virtual Python '
+                            'environments in one or '
+                            'more target '
+                            'directories.',
+                epilog='Once an environment has been '
+                        'created, you may wish to '
+                        'activate it, e.g. by '
+                        'sourcing an activate script '
+                        'in its bin directory.'
+                )
+
+    print("main() was executed")
+
+def setup():
+    pass
+    
+
+>>>>>>> Stashed changes
--- src/data_mgmt/__main__.py
+++ src/data_mgmt/__main__.py
@@ -1,2 +1,15 @@
+<<<<<<< Updated upstream
 
 from . import *
+=======
+import sys
+from . import main, setup
+
+rc = 1
+try:
+    main()
+    rc = 0
+except Exception as e:
+    print('Error: %s' % e, file=sys.stderr)
+sys.exit(rc)
+>>>>>>> Stashed changes
